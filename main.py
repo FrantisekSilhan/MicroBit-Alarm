7750bdecdfbead6f610f2af54d99da7591f6b401
František Šilhán <91247814+FrantisekSilhan@users.noreply.github.com>
2022-03-10 11:24:18 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.py
@@ -0,0 +1,49 @@
+radio.set_group(1)
+radio.set_transmit_power(7)
+radio.set_transmit_serial_number(True)
+
+learning = 0
+data_list = [control.device_serial_number()]
+
+#funkce
+radio.on_received_value(received)
+input.on_button_pressed(Button.A, on_alarm)
+input.on_button_pressed(Button.B, off_alarm)
+input.on_logo_event(TouchButtonEvent.PRESSED, send_learn)
+input.on_logo_event(TouchButtonEvent.LONG_PRESSED, learn)
+
+
+def received(name, value):
+    global learning, data_list
+    remote_serial = radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+    console.log_value("remote", remote_serial)
+    if learning == 1:
+        if name == "learn" and value == 1:
+            learned_serial = radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+            if learned_serial not in data_list:
+                data_list.append(learned_serial)
+            console.log_value("learned", remote_serial)
+            basic.clear_screen()
+            learning = 0
+    for serialek in data_list:
+        if remote_serial == serialek:
+            if name == "alarm" and value == 1:
+                music.play_tone(Note.C, 0)
+            elif name == "alarm" and value == 0:
+                music.stop_all_sounds()
+        print("remote_serial: "+remote_serial+" learned_serial: "+learned_serial)
+
+
+def on_alarm():
+    radio.sendValue("alarm", 1)
+
+def off_alarm():
+    radio.sendValue("alarm", 0)
+
+def learn():
+    global learning
+    basic.show_icon(IconNames.YES)
+    learning = 1
+
+def send_learn():
+    radio.sendValue("learn", 1)
---
+++ main.ts
@@ -0,0 +1,52 @@
+radio.setGroup(1)
+radio.setTransmitPower(7)
+radio.setTransmitSerialNumber(true)
+let learning = 0
+let data_list = [control.deviceSerialNumber()]
+// funkce
+radio.onReceivedValue(function received(name: string, value: number) {
+    let learned_serial: number;
+    
+    let remote_serial = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+    console.logValue("remote", remote_serial)
+    if (learning == 1) {
+        if (name == "learn" && value == 1) {
+            learned_serial = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+            if (data_list.indexOf(learned_serial) < 0) {
+                data_list.push(learned_serial)
+            }
+            
+            console.logValue("learned", remote_serial)
+            basic.clearScreen()
+            learning = 0
+        }
+        
+    }
+    
+    for (let serialek of data_list) {
+        if (remote_serial == serialek) {
+            if (name == "alarm" && value == 1) {
+                music.playTone(Note.C, 0)
+            } else if (name == "alarm" && value == 0) {
+                music.stopAllSounds()
+            }
+            
+        }
+        
+        console.log("remote_serial: " + remote_serial + " learned_serial: " + learned_serial)
+    }
+})
+input.onButtonPressed(Button.A, function on_alarm() {
+    radio.sendValue("alarm", 1)
+})
+input.onButtonPressed(Button.B, function off_alarm() {
+    radio.sendValue("alarm", 0)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function send_learn() {
+    radio.sendValue("learn", 1)
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function learn() {
+    
+    basic.showIcon(IconNames.Yes)
+    learning = 1
+})
